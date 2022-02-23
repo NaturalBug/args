bec4a269905ca7f0a819de197ff814108bd1dce9
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-02-23 20:15:36 +0800

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (ArgsException e) {
+            System.out.printf("Argument error: %s\n", e.errorMessage());
+        }
+    }
+
+    private static void executeApplication(boolean logging, int port, String directory) throws Exception {
+    }
+}
